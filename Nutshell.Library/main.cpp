857069f918dd2419a3b85f2117fec29dd1648835
kirill-korolev <kafafyf@gmail.com>
2017-11-06 13:13:54 +0300

    Initial Commit

---
+++ Nutshell.Library/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Nutshell.Library
+//
+//  Created by Kirill Korolev on 06/11/2017.
+//  Copyright © 2017 Kirill Korolev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
