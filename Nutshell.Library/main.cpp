22cd32a8459e77729e0040055fc48e10d1629269
kirill-korolev <kafafyf@gmail.com>
2017-11-08 03:06:09 +0300

    Camera class has been added. Basic configurations of a camera can be found in a config file.

--- Nutshell.Library/main.cpp
+++ Nutshell.Library/main.cpp
@@ -7,9 +7,12 @@
 //
 
 #include <iostream>
+#include "nutshell/camera.hpp"
+
+using namespace nutshell::camera;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    Camera camera("send");
+    camera.capture();
     return 0;
 }
